7a4851ca43c9d7d2cc8a5a7a9650bdc27bae6a95
Jared Peter <aljapeter05@gmail.com>
2024-12-27 19:24:45 -0300

    added edit functionality

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -1,4 +1,22 @@
+import { createForm, required } from "@modular-forms/solid";
+import SubmitButton from "./SubmitButton";
+import { createSignal } from "solid-js";
+import { csrf, request } from "../lib/utils";
+
 export default function Company(props) {
+    const [editForm, { Form, Field }] = createForm();
+    const [name, setName] = createSignal(props.name)
+    const [director, setDirector] = createSignal(props.director)
+    const [actors, setActors] = createSignal(props.actors)
+
+    const handleEdit = async (values, event) => {
+        await csrf();
+        await request.put('/companies/' + props.companyId, values);
+
+        document.getElementById(props.id + '_form').submit();
+        props.refetchData();
+    }
+
     return (
         <div className="my-3">
             <details class="collapse rounded-b-none rounded-md w-full bg-gray-50 collapse-arrow">
@@ -12,8 +30,91 @@ export default function Company(props) {
                 </div>
             </details>
             <div className="flex text-sm font-bold bg-gray-50 rounded-t-none rounded-md pb-2.5 pl-4">
-                <span className="text-blue-600">Editar</span> 
-                <span className="text-red-600 ml-2">Eliminar</span> 
+                <button class="text-blue-600" onClick={() => document.getElementById(props.id).showModal()}>
+                    Editar
+                </button>
+                <dialog id={props.id} className="modal">
+                    <div className="modal-box">
+                        <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className=" absolute right-5 top-3">✕</button>
+                        </form>
+                        <h3 className="font-bold text-lg">{props.name}</h3>
+                        <div className="py-2">
+                            <Form onSubmit={handleEdit}>
+                                <Field
+                                    name="name"
+                                    validate={[
+                                        required('Este campo es obligatorio')
+                                    ]}
+                                >
+                                    {(field, props) =>
+                                        <>
+                                            <input
+                                                {...props}
+                                                type="name"
+                                                class="form-input"
+                                                placeholder="Nombre de la compania"
+                                                value={name()}
+                                            />
+                                            {field.error && <div class="form-error">{field.error}</div>}
+                                        </>
+                                    }
+                                </Field>
+                                <Field
+                                    name="director"
+                                    validate={[
+                                        required('Este campo es obligatorio')
+                                    ]}
+                                >
+                                    {(field, props) =>
+                                        <>
+                                            <input
+                                                {...props}
+                                                type="director"
+                                                class="form-input"
+                                                placeholder="Director"
+                                                value={director()}
+                                            />
+                                            {field.error && <div class="form-error">{field.error}</div>}
+                                        </>
+                                    }
+                                </Field>
+                                <Field
+                                    name="actors"
+                                    validate={[
+                                        required('Este campo es obligatorio')
+                                    ]}
+                                >
+                                    {(field, props) =>
+                                        <>
+                                            <input
+                                                {...props}
+                                                type="actores"
+                                                class="form-input"
+                                                placeholder="Actores"
+                                                value={actors()}
+                                            />
+                                            {field.error && <div class="form-error">{field.error}</div>}
+                                        </>
+                                    }
+                                </Field>
+
+                                <SubmitButton
+                                    type="submit"
+                                    class="form-button"
+                                    loadingVariable={editForm.submitting}
+                                >
+                                    Subir cambios
+                                </SubmitButton>
+                            </Form>
+                        </div>
+                    </div>
+                    <form method="dialog" id={props.id + '_form'} className="modal-backdrop">
+                        <button>close</button>
+                    </form>
+                </dialog>
+                <span className="text-red-600 ml-2">Eliminar</span>
             </div>
         </div>
     );
--- src/routes/companies/index.jsx
+++ src/routes/companies/index.jsx
@@ -8,7 +8,10 @@ const fetchCompanies = async () => {
 }
 
 export default function Companies() {
-    const [companies] = createResource(fetchCompanies);
+    const [refetch, setRefetch] = createSignal(0);
+    const [companies] = createResource(refetch, fetchCompanies);
+
+    const refetchData = () => setRefetch(prevRefetch => prevRefetch + 1);
 
     return (
         <>
@@ -30,40 +33,14 @@ export default function Companies() {
                 <Match when={companies()}>
                     <For each={companies()}>
                         {(item, index) =>
-                            <Company name={item.name} director={item.director} actors={item.actors} />
-                            // <div class="my-3 px-4 py-3 bg-gray-50 rounded-md w-full">
-                            //     <div className="flex justify-between">
-                            //         <div>
-                            //             <div className="font-bold">{item.name}</div>
-                            //             <div class="text-sm">{item.director}</div>
-                            //         </div>
-                            //         <div>
-                            //             <button onClick={() => document.getElementById(index).showModal()}>
-                            //                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            //                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
-                            //                 </svg>
-                            //             </button>
-                            //             <dialog id={index} className="modal">
-                            //                 <div className="modal-box">
-                            //                     <form method="dialog">
-                            //                         {/* if there is a button in form, it will close the modal */}
-                            //                         <button className=" absolute right-5 top-3">✕</button>
-                            //                     </form>
-                            //                     <h3 className="font-bold text-lg">{item.name}</h3>
-                            //                     <p className="py-4"><span className="font-bold">
-                            //                         Director: </span> {item.director}
-                            //                         <br />
-                            //                         <span className="font-bold">Actores: </span> {item.actors}
-                            //                     </p>
-                            //                     <p></p>
-                            //                 </div>
-                            //                 <form method="dialog" className="modal-backdrop">
-                            //                     <button>close</button>
-                            //                 </form>
-                            //             </dialog>
-                            //         </div>
-                            //     </div>
-                            // </div>
+                            <Company
+                                refetchData={refetchData}
+                                id={index}
+                                companyId={item.id}
+                                name={item.name}
+                                director={item.director}
+                                actors={item.actors}
+                            />
                         }
                     </For>
                 </Match>
