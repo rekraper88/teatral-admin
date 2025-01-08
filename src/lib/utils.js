c899506165a9010f5df9d680324a0a0a5d0587fa
Jared Peter <aljapeter05@gmail.com>
2025-01-07 21:15:44 -0300

    test

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,60 +1,6 @@
-// export default async (url, method = 'get', data = null, credentials = 'include') => {
-//     try {
-//         const xsrfToken = document.cookie
-//             .split('; ')
-//             .find(row => row.startsWith('XSRF-TOKEN='))
-//             ?.split('=')[1];
 
 import axios from "axios";
 
-//         const response = await fetch(url, {
-//             method,
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 ...(xsrfToken ? { 'X-XSRF-TOKEN': decodeURIComponent(xsrfToken) } : {}),
-//             },
-//             ...(data ? { body: JSON.stringify(data) } : {}),
-//             credentials
-//         });
-
-//         if (!response.ok) {
-//             const errorMessage = await response.text();
-//             return errorMessage;
-//         }
-
-//         return response.json();
-//     } catch (error) {
-//         return error;
-//     }
-// };
-
-// export axios.create
-
-const API_PATH='http://localhost:8000';
-
-// export const request = {
-//     post: async (url, data) => {
-//         try {
-//             const response = await fetch(`${API_PATH}${url.startsWith('/') ? url : `/${url}`}`, {
-//                 method: 'POST',
-//                 body: JSON.stringify(data),
-//                 credentials: 'include',
-//             });
-
-//             if (!response.ok) {
-//                 const errorMessage = await response.text();
-//                 return errorMessage;
-//             }
-
-//             return response.json();
-//         } catch (error) {
-//             return error;
-//         }
-//     }
-// }
-
-        
-        
 
 
 export const request = axios.create({
@@ -82,9 +28,12 @@ request.interceptors.request.use(async (config) => {
 });
 
 export const csrf = async () => {
-    await axios.get('https://teatral-api-production.up.railway.app/sanctum/csrf-cookie', {
+    const res = await axios.get('https://teatral-api-production.up.railway.app/sanctum/csrf-cookie', {
         withCredentials: true
     });
+    console.log(res, document.cookie);
+    return res;
+    
 };
 
 
