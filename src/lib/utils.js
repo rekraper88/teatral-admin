88676e7fec22c1ac46a72102851e926f632689e7
Jared Peter <aljapeter05@gmail.com>
2025-01-07 21:42:45 -0300

    typo fix for second test

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -63,4 +63,4 @@ export const request = axios.create({
     withXSRFToken: true
 });
 
-export const csrf = async () => await request.get('http://localhost:8000/sanctum/csrf-cookie');
+export const csrf = async () => await request.get('https://teatral-api-production.up.railway.app/sanctum/csrf-cookie');
